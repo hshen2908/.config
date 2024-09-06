1dc5ff89d238087faf4183faea3adb096dd80f9c
Henrique Shen <hshen2908@gmail.com>
2024-09-06 14:28:14 -0700

    Add fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,9 @@ alias ls "eza --hyperlink --git --all" #--absolute
 alias lst "eza --tree --recurse --level 2 --hyperlink --git --all" #--absolute
 alias lsa "eza --tree --recurse --level 2 --hyperlink --long --header --links --inode --mounts --blocksize --total-size --modified --git --extended --all" #--absolute
 
+alias cdc "cd ~/.config/"
+alias cds "cd /mnt/share/"
+
 # pokemon-colorscripts --no-title -r
 
 starship init fish | source
