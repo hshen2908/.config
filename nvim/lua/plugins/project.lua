45be66e6444213b3ffc5faaf0b7bb0773e486c75
Henrique Shen <hshen2908@gmail.com>
2024-09-17 18:33:48 -0700

    Update project pattern

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -5,7 +5,7 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
-            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.typ", ">notes" },
+            patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "main.typ", ">notes", ">assignments" },
             show_hidden = true,
         }
     end
